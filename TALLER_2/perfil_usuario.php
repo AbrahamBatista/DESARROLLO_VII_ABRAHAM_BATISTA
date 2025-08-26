f47124867507a2776a247e2f28598329afc2bebb
AbrahamBatista <abraham.batista@utp.ac.pa>
2025-08-25 20:59:55 -0500

    Update perfil_usuario.php

--- TALLER_2/perfil_usuario.php
+++ TALLER_2/perfil_usuario.php
@@ -1,30 +1,19 @@
 <?php
-// Definición de variables
+
 $nombre_completo = "Abraham Batista";
 $edad = 24;
 $correo = "Abraham.Batista@utp.ac.pa";
 $telefono = "+507 6000-0000";
-
-// Definición de constante
 define("OCUPACION", "Estudiante");
 
-// Uso de echo
 echo "<p>Nombre completo: " . $nombre_completo . "</p>";
 echo "<p>Edad: $edad</p>";
 
-// Uso de print
 print "<p>Correo electrónico: $correo</p>";
-
-// Uso de printf (con formato)
 printf("<p>Teléfono: %s</p>", $telefono);
-
-// Constante
 echo "<p>Ocupación: " . OCUPACION . "</p>";
-
-// Salto de línea
 echo "<hr>";
 
-// Uso de var_dump para mostrar tipo y valor
 var_dump($nombre_completo);
 echo "<br>";
 var_dump($edad);
@@ -42,3 +31,4 @@ git add TALLERES/TALLER_2/perfil_usuario.php
 git commit -m "Ejercicio de perfil de usuario completado"
 git push origin main
                     
+
