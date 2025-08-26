70d408460fa212a96d85f5cef3558e35fa4fd096
AbrahamBatista <abraham.batista@utp.ac.pa>
2025-08-25 20:58:22 -0500

    Update index.php

--- TALLER_2/CATALOGO_LIBROS/index.php
+++ TALLER_2/CATALOGO_LIBROS/index.php
@@ -2,13 +2,11 @@
 require_once 'includes/funciones.php';
 include 'includes/encabezado.php';
 
-// Obtener la lista de libros
 $libros = obtenerLibros();
-
-// Mostrar los detalles de cada libro
 foreach ($libros as $libro) {
     echo mostrarDetallesLibro($libro);
 }
 
 include 'includes/pie_pagina.php';
 ?>
+
