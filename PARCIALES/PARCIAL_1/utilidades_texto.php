c3ca155a73203d969b12493bfdb329e6e939a4a3
AbrahamBatista <abraham.batista@utp.ac.pa>
2025-09-08 23:31:48 +0000

    Agregar funciones de utilidades de texto para el parcial

---
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -0,0 +1,22 @@
+<?php
+// utilidades_texto.php
+
+// Función 1: contar_palabras
+function contar_palabras($texto) {
+    return str_word_count($texto);
+}
+
+// Función 2: contar_vocales
+function contar_vocales($texto) {
+    $texto = strtolower($texto);
+    preg_match_all('/[aeiou]/', $texto, $coincidencias);
+    return count($coincidencias[0]);
+}
+
+// Función 3: invertir_palabras
+function invertir_palabras($texto) {
+    $palabras = explode(" ", $texto);
+    $palabras = array_reverse($palabras);
+    return implode(" ", $palabras);
+}
+?>
