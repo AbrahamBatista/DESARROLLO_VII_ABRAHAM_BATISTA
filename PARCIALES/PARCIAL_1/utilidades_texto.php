34144e4dd956a19828361979952844c94a1a4b0a
AbrahamBatista <abraham.batista@utp.ac.pa>
2025-09-09 00:02:29 +0000

    Agregando funciones de utilidades de texto (correciones)

--- PARCIALES/PARCIAL_1/utilidades_texto.php
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -1,22 +1,25 @@
 <?php
 
 
-// Función 1: contar_palabras
+// Funcion 1: contar_palabras
 function contar_palabras($texto) {
     return str_word_count($texto);
 }
 
-// Función 2: contar_vocales
+// Funcion 2: contar_vocales
 function contar_vocales($texto) {
     $texto = strtolower($texto);
     preg_match_all('/[aeiou]/', $texto, $coincidencias);
     return count($coincidencias[0]);
 }
 
-// Función 3: invertir_palabras
+// Funcion 3: invertir_palabras
 function invertir_palabras($texto) {
     $palabras = explode(" ", $texto);
     $palabras = array_reverse($palabras);
     return implode(" ", $palabras);
 }
 ?>
+
+
+          
