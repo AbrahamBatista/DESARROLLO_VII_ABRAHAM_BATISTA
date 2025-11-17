717ae4d834ad3442828df838af46c4377a0b3dd6
AbrahamBatista <Abraham.batista@utp.ac.pa>
2025-11-17 18:52:39 -0500

    agregar datos que voy a usar

--- PARCIALES/PARCIAL_3/datos.php
+++ PARCIALES/PARCIAL_3/datos.php
@@ -1,1 +1,15 @@
+<?php
+// Usuarios y contraseñas (roles)
+$usuarios = [
+    "profesor1" => ["password" => "admin123", "rol" => "Profesor"],
+    "juan23"    => ["password" => "clave123", "rol" => "Estudiante"],
+    "maria77"   => ["password" => "pass567", "rol" => "Estudiante"],
+];
+
+// Calificaciones solo para estudiantes
+$calificaciones = [
+    "juan23" => 88,
+    "maria77" => 95,
+];
+?>
 
