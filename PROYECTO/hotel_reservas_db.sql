7858c496d58837d3ff8559e90b2d40c08b2021c1
AbrahamBatista <Abraham.batista@utp.ac.pa>
2025-12-10 18:33:46 -0500

    implementando ultimas modificaciones

--- PROYECTO/hotel_reservas_db.sql
+++ PROYECTO/hotel_reservas_db.sql
@@ -1,35 +1,46 @@
-CREATE DATABASE hotel_reservas_db;
+CREATE DATABASE IF NOT EXISTS hotel_reservas_db;
 USE hotel_reservas_db;
 
+
+
 CREATE TABLE habitaciones (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100),
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(120) NOT NULL,
     descripcion TEXT,
-    capacidad INT,
-    precio DECIMAL(10,2)
+    capacidad INT NOT NULL,
+    precio DECIMAL(10,2) NOT NULL
 );
 
+
+
+CREATE TABLE servicios (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(120) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL
+);
+
+
+
 CREATE TABLE reservas (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    habitacion_id INT,
-    nombre_cliente VARCHAR(120),
-    fecha_entrada DATE,
-    fecha_salida DATE,
-    total DECIMAL(10,2),
-    estado VARCHAR(20) DEFAULT 'Pagado',
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    habitacion_id INT NOT NULL,
+    nombre_cliente VARCHAR(120) NOT NULL,
+    email_cliente VARCHAR(120) NOT NULL,   
+    fecha_entrada DATE NOT NULL,
+    fecha_salida DATE NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
+    estado VARCHAR(50) NOT NULL DEFAULT 'Confirmada',
+
     FOREIGN KEY (habitacion_id) REFERENCES habitaciones(id)
 );
 
-CREATE TABLE servicios (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    precio DECIMAL(10,2)
-);
+
 
 CREATE TABLE reserva_servicios (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    reserva_id INT,
-    servicio_id INT,
-    FOREIGN KEY (reserva_id) REFERENCES reservas(id),
-    FOREIGN KEY (servicio_id) REFERENCES servicios(id)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    reserva_id INT NOT NULL,
+    servicio_id INT NOT NULL,
+
+    FOREIGN KEY (reserva_id) REFERENCES reservas(id) ON DELETE CASCADE,
+    FOREIGN KEY (servicio_id) REFERENCES servicios(id) ON DELETE CASCADE
 );
